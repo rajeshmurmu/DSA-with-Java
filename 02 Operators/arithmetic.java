1196b272281acf728cb494f01d91ef3c9daf3747
Rajesh Murmu <rajeshsonmurmur2017@gmail.com>
2023-12-29 22:55:40 +0530

    Flowcharts and pseudocode

---
+++ 02 Operators/arithmetic.java
@@ -0,0 +1,41 @@
+public class arithmetic{
+    public static void main(String args[]){
+        // Binary operators
+        // int A = 10;
+        // int B = 5;
+        // System.out.println("Add = " + (A+B));
+        // System.out.println("Subtract = " + (A-B));
+        // System.out.println("Multiplication = " + (A*B));
+        // System.out.println("Division = " + (A/B));
+        // System.out.println("Modulo = " + (A%B));
+
+        // Unary operators
+
+        // Pre Increament
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Post Increament
+        // int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Pre Decreament
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Post Decreament
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+}
---
+++ 02 Operators/logical.java
@@ -0,0 +1,13 @@
+public class logical{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 10;
+
+
+        // logical AND(&&) logical OR(||) logical !(NOT)
+        System.out.println((a>b)&&(b<a));
+        System.out.println((a>b)||(b<a));
+        System.out.println(!false);
+    }
+}
