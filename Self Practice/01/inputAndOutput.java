202c3b58d51b9e04f8d551534f649cf48205c48f
Rajesh Murmu <rajeshsonmurmur2017@gmail.com>
2023-12-31 19:17:05 +0530

    self practice folder added

---
+++ Self Practice/01/inputAndOutput.java
@@ -0,0 +1,23 @@
+// Link:- https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+import java.util.Scanner;
+public class inputAndOutput{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        int c = a;
+
+        if(c>=65 && c<=90)
+        {
+            System.out.print(1);
+        }
+        else if(c>=97 && c<=122)
+        {
+            System.out.print(0);
+        }
+        else{
+            System.out.print(-1);
+        }
+    }
+}
