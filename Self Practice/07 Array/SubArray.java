89b6fca589978e247b482f85839447ecfc01a791
Rajesh Murmu <rajeshsonmurmur2017@gmail.com>
2024-02-13 00:08:40 +0530

    add some new code to SubArray.java file

--- Self Practice/07 Array/SubArray.java
+++ Self Practice/07 Array/SubArray.java
@@ -1,31 +1,40 @@
 
 public class SubArray {
 
-    public static void subArrays(int arr[])
-    {
-        int max= Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++)
-        {
+    public static void subArrays(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
             // System.out.println("subarrays"+i);
-            for(int j=i;j<arr.length;j++)
-            {
-                int sum=0;
-                for(int k=i;k<=j;k++)
-                {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
                     // System.out.print(arr[k]+" ");
-                    sum+=arr[k];
+                    sum += arr[k];
 
                 }
                 // System.out.println();
-                if(sum>max) max=sum;
+                if (sum > max)
+                    max = sum;
             }
 
         }
         System.out.println(max);
     }
 
+    public static void findSubArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + ",");
+                }
+                System.out.println();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        subArrays(arr);
+        int arr[] = { 1, 2, 3, 4, 5 };
+        // subArrays(arr);
+        findSubArray(arr);
     }
 }
